ec4306276ca9201ee01cae48e14a23dfa3d93301
Vansh Soni <vanshsoni16@gmail.com>
2018-01-03 15:31:49 +0530

    New Contact Page

--- contact.php
+++ contact.php
@@ -24,18 +24,199 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE
 -->
 <?php include ('includes/header.php'); ?>
+<style>
+
+
+
+
+.finer-print {
+  font-size: 15px;
+}
+
+.span2 {
+  width: 100%;
+  padding: 0;
+  float: left;
+}
+
+.business-card {
+  background: #eeeeec;
+  display:inline-block;
+  color: #2e3436;
+  width: 300px;
+  height: 180px;
+  font-size: 16px;
+  border-bottom: 2px solid #d3d7df;
+  border-radius: 15px;
+  box-shadow: 0 0 10px 1px #000;
+  margin-top: 5%;
+  margin-left:60px;
+}
+
+.business-card .title {
+  background: #c00;
+  color: #fff;
+  height: 30px;
+  padding: 10px;
+  font-weight: bold;
+  font-size: 14px;
+  border-top: 2px solid #ef2929;
+  border-left: 2px solid #ef2929;
+  border-radius: 14px 14px 0 0;
+}
+
+.business-card .content {
+  font-weight: bold;
+  padding: 15px;
+}
+
+
+
+</style>
 <div id="content">
     <div id="info1"class="info">
-        <h1>   Contacts</h1>
-        <p>President, Students’ Gymkhana : Dr. Sahid Hussain - 0612-2552022, Email: presidentsg[AT]iitp.ac.in</p>
-        <p>Treasurer, Students’ Gymkhana : Dr. Anup Kumar Keshri - 0612-2260258, Email: anup[AT]iitp.ac.in</p>
-        <p>Vice President, Students’ Gymkhana : Mr. K. Vidya Dutt - +91-8298679086, Email: vpgymkhana[AT]iitp.ac.in</p>
-        <p>General Secretary, Student Welfare and Technical Affairs : Mr. Siddharth Raina - +91-8507801416 ,Email: siddharthraina.me11[AT]iitp.ac.in</p>
-        <p>General Secretary, Cultural Affairs : Mr. NIkhil Singh - +91-8877859503, Email: nikhil.me11[AT]iitp.ac.in</p>
-        <p>General Secretary, Sports : Mr. Venkatesh Chaturvedi- +91-8544130919, Email: venkatesh.ee11[AT]iitp.ac.in</p>
-        <p>Postgraduate Representative : Mr. G. Lloyds Raja - +91-9122996724, Email: lloyds[AT]iitp.ac.in</p>
-        <strong>WebMaster: Anupam Das 1401CS52 - +91-9709313113 , Email: anupam.ee14[AT]iitp.ac.in </strong>
-    </div>
+	<div>
+	
+	
+	
+	
+<div class="business-card">
+  <div class="title">
+    President Student's Gymkhana
+  </div>
+  <div class="content span2">
+    Dr. Sahid Hussain<br />
+    <span class="finer-print">Mobile Number - 0612-2552022<br />
+       Email: presidentsg@iitp.ac.in<br />
+    </span>
+  </div>
+  <div class="content span2">
+    
+  </div>
+</div>
 
+
+
+
+
+<div class="business-card">
+  <div class="title">
+    Treasurer, Student's Gymkhana
+  </div>
+  <div class="content span2">
+    Dr. Anup Kumar Keshri<br />
+    <span class="finer-print">Mobile Number - 0612-2260258<br />
+       Email: anup@iitp.ac.in<br />
+    </span>
+  </div>
+  <div class="content span2">
+    
+  </div>
+</div>
+
+
+
+<div class="business-card">
+  <div class="title">
+    Vice President, Student's Gymkhana
+  </div>
+  <div class="content span2">
+    Mr. K. Vidya Dutt<br />
+    <span class="finer-print">Mobile Number - 91-8298679086<br />
+       Email: vpgymkhana@iitp.ac.in<br />
+    </span>
+  </div>
+  <div class="content span2">
+    
+  </div>
+</div>
+
+
+
+
+
+<div class="business-card">
+  <div class="title">
+    General Secretary, Technical Affairs
+  </div>
+  <div class="content span2">
+    Mr. Siddharth Raina <br />
+    <span class="finer-print">Mobile Number - 91-8507801416<br />
+       Email: siddharthraina.me11@iitp.ac.in<br />
+    </span>
+  </div>
+  <div class="content span2">
+    
+  </div>
+</div>
+
+<div class="business-card">
+  <div class="title">
+    General Secretary, Cultural Affairs
+  </div>
+  <div class="content span2">
+    Mr. Nikhil Singh <br />
+    <span class="finer-print">Mobile Number - 91-8877859503<br />
+       Email: nikhil.me11@iitp.ac.in<br />
+    </span>
+  </div>
+  <div class="content span2">
+    
+  </div>
+</div>
+
+
+
+
+<div class="business-card">
+  <div class="title">
+    General Secretary, Sports
+  </div>
+  <div class="content span2">
+    Mr. Venkatesh Chaturvedi <br />
+    <span class="finer-print">Mobile Number - 91-8544130919<br />
+       Email: venkatesh.ee11@iitp.ac.in<br />
+    </span>
+  </div>
+  <div class="content span2">
+    
+  </div>
+</div>
+
+
+<div class="business-card">
+  <div class="title">
+   Postgraduate Representative
+  </div>
+  <div class="content span2">
+    Mr. G. Lloyds Raja <br />
+    <span class="finer-print">Mobile Number - 91-9122996724<br />
+       Email: lloyds@iitp.ac.in<br />
+    </span>
+  </div>
+  <div class="content span2">
+    
+  </div>
+</div>
+
+
+
+<div class="business-card">
+  <div class="title">
+   WebMaster
+  </div>
+  <div class="content span2">
+    Anupam Das <br />
+    <span class="finer-print">Mobile Number - 91-9709313113<br />
+       Email: anupam.ee14@iitp.ac.in<br />
+    </span>
+  </div>
+  <div class="content span2">
+    
+  </div>
+</div>
+
+
+</div>
 </div>
 <?php include ('includes/footer.php'); ?>
