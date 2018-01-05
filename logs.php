80465abef0286440972be1f7126d8041c824d492
Vansh Soni <vanshsoni16@gmail.com>
2018-01-05 21:48:17 +0530

    Updated Login Page UI

--- logs.php
+++ logs.php
@@ -29,9 +29,101 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE
 -->
+<style>
+@import url(https://fonts.googleapis.com/css?family=Roboto:300);
+
+.login-page {
+  width: 360px;
+  padding: 2% 0 0;
+  margin: auto;
+}
+.form {
+  position: relative;
+  z-index: 1;
+  background: #FFFFFF;
+  max-width: 360px;
+  margin: 0 auto 0px;
+  padding: 20px;
+  text-align: center;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+}
+.input {
+  font-family: "Roboto", sans-serif;
+  outline: 0;
+  background: #f2f2f2;
+  width: 100%;
+  border: 0;
+  margin: 0 0 15px;
+  padding: 15px;
+  box-sizing: border-box;
+  font-size: 14px;
+}
+.submit {
+  font-family: "Roboto", sans-serif;
+  text-transform: uppercase;
+  outline: 0;
+  background: #4CAF50;
+  width: 100%;
+  border: 0;
+  padding: 15px;
+  color: #FFFFFF;
+  font-size: 14px;
+  -webkit-transition: all 0.3 ease;
+  transition: all 0.3 ease;
+  cursor: pointer;
+}
+.submit:hover,.submit:active,.submit:focus {
+  background: #43A047;
+}
+.form .message {
+  margin: 15px 0 0;
+  color: #b3b3b3;
+  font-size: 12px;
+}
+.form .message a {
+  color: #4CAF50;
+  text-decoration: none;
+}
+.form .register-form {
+  display: none;
+}
+.container {
+  position: relative;
+  z-index: 1;
+  max-width: 300px;
+  margin: 0 auto;
+}
+.container:before, .container:after {
+  content: "";
+  display: block;
+  clear: both;
+}
+.container .info {
+  margin: 50px auto;
+  text-align: center;
+}
+.container .info h1 {
+  margin: 0 0 15px;
+  padding: 0;
+  font-size: 36px;
+  font-weight: 300;
+  color: #1a1a1a;
+}
+.container .info span {
+  color: #4d4d4d;
+  font-size: 12px;
+}
+.container .info span a {
+  color: #000000;
+  text-decoration: none;
+}
+.container .info span .fa {
+  color: #EF3B3A;
+}
+
+</style>
 <?php include ('includes/header.php'); ?>
-<div class="login">
-    <h2 class="bar">Please Sign In</h2>
+
     <?php
     if (($_SERVER['REQUEST_METHOD'] == 'POST')):
         if (isset($_POST['roll']) && isset($_POST['password'])) {
@@ -62,15 +154,25 @@ THE SOFTWARE
 
     endif;
     ?>
-    <form action="logs.php" method="POST">
-        <input type="text" tabindex="1" maxlength="10" autofocus placeholder="Roll No. *" required name="roll" value="<?php
+
+	
+	
+	<div class="login-page">
+	<div class="form">
+	<h2>Please Sign In</h2>
+	  </div>
+  <div class="form">
+    <form action="logs.php" method="POST" class="login-form">
+        <input type="text" class="input" tabindex="1" maxlength="10" autofocus placeholder="Roll No. *" required name="roll" value="<?php
         if (isset($roll) && !empty($roll)) {
             echo $roll;
         }
         ?>">
-        <input type="password" placeholder="Password  * " maxlength="40"required tabindex="2" name="password">
-        <input type="submit" tabindex="3" value="LOGIN">
+        <input type="password" class="input" placeholder="Password  * " maxlength="40"required tabindex="2" name="password">
+        <input type="submit" class="submit" tabindex="3" value="LOGIN">
+		    <p>Not Yet Registered?<br><a tabindex="4" href="signs.php">Register Here</a></p>
     </form>
-    <p>Not Yet Registered?<br><a tabindex="4" href="signs.php">Register Here</a></p>
+  </div>
 </div>
+
 <?php include ('includes/footer.php'); ?>
