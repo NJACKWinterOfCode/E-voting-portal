f83170123e9dbe1583facc1f91236dadc57083c0
Vansh Soni <vanshsoni16@gmail.com>
2018-01-07 18:24:35 +0530

    Register and SignUp form (#15)
    Register and SignUp form with updated UI and email validation

--- logs.php
+++ logs.php
@@ -47,17 +47,7 @@ THE SOFTWARE
   text-align: center;
   box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
 }
-.input {
-  font-family: "Roboto", sans-serif;
-  outline: 0;
-  background: #f2f2f2;
-  width: 100%;
-  border: 0;
-  margin: 0 0 15px;
-  padding: 15px;
-  box-sizing: border-box;
-  font-size: 14px;
-}
+
 .submit {
   font-family: "Roboto", sans-serif;
   text-transform: uppercase;
@@ -121,7 +111,101 @@ THE SOFTWARE
   color: #EF3B3A;
 }
 
+
+
+@charset "utf-8";
+@import url(http://weloveiconfonts.com/api/?family=fontawesome);
+
+[class*="fontawesome-"]:before {
+  font-family: 'FontAwesome', sans-serif;
+}
+
+
+.input {
+  border: none;
+  font-family: 'Open Sans', Arial, sans-serif;
+  font-size: 16px;
+  line-height: 1.5em;
+  padding: 0;
+  -webkit-appearance: none;
+}
+
+after { clear: both; }
+
+#login {
+  margin: 20px auto;
+  width: 320px;
+}
+
+#login form {
+    margin-left: -20px;
+  padding: 22px 22px 22px 22px;
+  width: 100%;
+  border-radius: 5px;
+  background: #282e33;
+  border-top: 3px solid #434a52;
+  border-bottom: 3px solid #434a52;
+}
+
+#login form span {
+  background-color: #363b41;
+  border-radius: 3px 0px 0px 3px;
+  border-right: 3px solid #434a52;
+  color: #606468;
+  display: block;
+  float: left;
+  line-height: 50px;
+  text-align: center;
+  width: 50px;
+  height: 50px;
+}
+
+#login form input[type="text"] {
+  background-color: #3b4148;
+  border-radius: 0px 3px 3px 0px;
+  color: #a9a9a9;
+  margin-bottom: 1em;
+  padding: 0 16px;
+  width: 82%;
+  height: 50px;
+}
+
+#login form input[type="password"] {
+  background-color: #3b4148;
+  border-radius: 0px 3px 3px 0px;
+  color: #a9a9a9;
+  margin-bottom: 1em;
+  padding: 0 16px;
+  width: 82%;
+  height: 50px;
+}
+
+#login form input[type="submit"] {
+  background: #16aa56;
+  border: 0;
+  width: 100%;
+  height: 40px;
+  border-radius: 3px;
+  border-style: inset;
+  color: white;
+  cursor: pointer;
+  transition: background 0.3s ease-in-out;
+}
+#login form input[type="submit"]:hover {
+  background: #16aa56;
+}
+.register_button {
+  background: #16aa56;
+  border: 0;
+  width: 62%;
+  height: 40px;
+  border-radius: 3px;
+  cursor: pointer;
+  padding:15px 0px 0px 120px;
+  transition: background 0.3s ease-in-out;
+}
 </style>
+<script src="https://use.fontawesome.com/611b5cfd70.js"></script>
 <?php include ('includes/header.php'); ?>
 
     <?php
@@ -161,18 +245,23 @@ THE SOFTWARE
 	<div class="form">
 	<h2>Please Sign In</h2>
 	  </div>
-  <div class="form">
-    <form action="logs.php" method="POST" class="login-form">
-        <input type="text" class="input" tabindex="1" maxlength="10" autofocus placeholder="Roll No. *" required name="roll" value="<?php
+
+  <div id="login">
+      <form name='form-login' action="logs.php" method="POST" >
+        <span class="fontawesome-user"><i class="fa fa-user-circle-o fa-2x" aria-hidden="true" style="margin-top:10px;"></i></span>
+          <input type="text" class="input" tabindex="1" maxlength="10" autofocus placeholder="Roll No. *" required name="roll" value="<?php
         if (isset($roll) && !empty($roll)) {
             echo $roll;
         }
         ?>">
-        <input type="password" class="input" placeholder="Password  * " maxlength="40"required tabindex="2" name="password">
-        <input type="submit" class="submit" tabindex="3" value="LOGIN">
-		    <p>Not Yet Registered?<br><a tabindex="4" href="signs.php">Register Here</a></p>
-    </form>
-  </div>
+       
+        <span class="fontawesome-lock"><i class="fa fa-key fa-2x" aria-hidden="true" style="margin-top:10px;"></i></span>
+           <input type="password" class="input" placeholder="Password  * " maxlength="40"required tabindex="2" name="password">
+        <input type="submit" tabindex="3" value="LOGIN"><br>
+		    <p style="text-decoration:none; color:silver;">Not Yet Registered?<br></p><p class="register_button" ><a href="signs.php" style="text-decoration:none;color: white;">Register Here</a></p>
+</form>
+      </div>
+
 </div>
 
 <?php include ('includes/footer.php'); ?>
